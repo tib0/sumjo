4fd1ee087117b3facc9cafb5e0b6112055e5cda8
Tib0 <4764729+tib0@users.noreply.github.com>
2024-08-30 16:03:50 +0200

    Parse inference output

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,54 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Image, Button } from 'react-native'
+import { useEffect, useState } from 'react';
+import { StyleSheet, Image, Button } from 'react-native'
 import { useTensorflowModel } from 'react-native-fast-tflite'
 import {
-  //ImagePickerResponse,
-  //launchCamera,
   launchImageLibrary,
 } from 'react-native-image-picker';
-
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import ImageResizer from '@bam.tech/react-native-image-resizer';
 import { convertToRGB } from 'react-native-image-to-rgb';
-//import { useResizePlugin } from 'vision-camera-resize-plugin';
-import { useAssets } from 'expo-asset';
-//import { useFrameProcessor } from 'react-native-vision-camera';
+
+const resizeImage = async (uri: string, width: number, height: number) => {
+  try {
+    const response = await ImageResizer.createResizedImage(uri, width, height, 'JPEG', 100, 0, undefined, true, { mode: "stretch" });
+    console.log(response.height + "x" + response.width, " img");
+    return response.uri;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
+function iou(box1: any, box2: any) {
+  return intersection(box1, box2) / union(box1, box2);
+}
+
+function union(box1: any, box2: any) {
+  const { box1_x1, box1_y1, box1_x2, box1_y2 } = box1;
+  const { box2_x1, box2_y1, box2_x2, box2_y2 } = box2;
+  const box1_area = (box1_x2 - box1_x1) * (box1_y2 - box1_y1);
+  const box2_area = (box2_x2 - box2_x1) * (box2_y2 - box2_y1);
+  return box1_area + box2_area - intersection(box1, box2);
+}
+
+function intersection(box1: any, box2: any) {
+  const { box1_x1, box1_y1, box1_x2, box1_y2 } = box1;
+  const { box2_x1, box2_y1, box2_x2, box2_y2 } = box2;
+  const x1 = Math.max(box1_x1, box2_x1);
+  const y1 = Math.max(box1_y1, box2_y1);
+  const x2 = Math.min(box1_x2, box2_x2);
+  const y2 = Math.min(box1_y2, box2_y2);
+  return (x2 - x1) * (y2 - y1);
+}
 
 export default function HomeScreen() {
-  const model = useTensorflowModel(require('../../assets/models/sumjo8su_float32.tflite'), 'core-ml')//model path
-  const actualModel = model.state === 'loaded' ? model.model : undefined
-/*
-  const [image, setImage] = useState<ImagePickerResponse | null>();
-  const [result, setResult] = useState<string>('');
-*/
+  const model = useTensorflowModel(require('../../assets/models/sumjo8su_float32.tflite'), 'core-ml');
+  const actualModel = model.state === 'loaded' ? model.model : undefined;
+  const [imgSize, setImgSize] = useState({width: 0, height: 0});
+
   useEffect(() => {
     if (actualModel == null) return
     console.log(`Model loaded!`);
@@ -31,159 +56,86 @@ export default function HomeScreen() {
 
   console.log(`Model: ${model.state} (${model.model != null})`);
 
-  //const { resize } = useResizePlugin();
-
-  //const objectDetection = useTensorflowModel(require('object_detection.tflite'))
-/* 
-  const handleCanvas = (canvas: Canvas) => {
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'purple';
-    ctx.fillRect(0, 0, 100, 100);
-  } */
-
-  const resizeImage = async (uri: string, width: number, height: number) => {
-    try {
-      const response = await ImageResizer.createResizedImage(uri, width, height, 'JPEG', 100, 0, undefined, true, { mode: "stretch" });
-      console.log(response.height + "x" + response.width, " img");
-      return response.uri;
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  };
-  
- /*  const frameProcessor = useFrameProcessor((frame) => {
-    'worklet'
-    if (actualModel == null) return
-
-    // 1. Resize 4k Frame to 192x192x3 using vision-camera-resize-plugin
-    const resized = resize(frame, {
-      scale: {
-        width: 192,
-        height: 192,
-      },
-      pixelFormat: 'rgb',
-      dataType: 'uint8',
-    })
-
-    // 2. Run model with given input buffer synchronously
-    const outputs = actualModel.runSync([resized])
-
-    // 3. Interpret outputs accordingly
-    const detection_boxes = outputs[0]
-    const detection_classes = outputs[1]
-    const detection_scores = outputs[2]
-    const num_detections = outputs[3]
-    console.log(`Detected ${num_detections[0]} objects!`)
-
-    for (let i = 0; i < detection_boxes.length; i += 4) {
-      const confidence = detection_scores[i / 4]
-      if (confidence > 0.7) {
-        // 4. Draw a red box around the detected object!
-        const left = detection_boxes[i]
-        const top = detection_boxes[i + 1]
-        const right = detection_boxes[i + 2]
-        const bottom = detection_boxes[i + 3]
-        const rect = SkRect.Make(left, top, right, bottom)
-        handleCanvas.drawRect(rect, SkColors.Red)
-      }
-    }
-  }, [model]) */
-
-
   const uploadImage = async () => {
     launchImageLibrary({ mediaType: 'photo', maxWidth: 640, maxHeight: 640 },//model input dimensions 640x640
       async response => {
         if (!response.didCancel) {
           if (response.assets && response.assets.length > 0 && response.assets[0]?.uri) {
             console.log(response.assets[0]?.uri)
-            
+            Image.getSize(
+              response.assets[0]?.uri, 
+              (width, height) => {
+                console.log(`Image size: ${width} - ${height}`);
+                setImgSize({width, height})
+              }, (error) => {
+                console.error(`Couldn't get the image size: ${error.message}`);
+              });
             const resized = await resizeImage(response.assets[0].uri, 640, 640)
             if (!resized) return;
-
             const convertedArray = await convertToRGB(resized);
-            let red = []
-            let blue = []
-            let green = []
+            let red = [];
+            let blue = [];
+            let green = [];
             for (let index = 0; index < convertedArray.length; index += 3) {
               red.push(convertedArray[index] / 255);
               green.push(convertedArray[index + 1] / 255);
               blue.push(convertedArray[index + 2] / 255);
             }
             const finalArray = [...red, ...green, ...blue];
-            //convert to Uint8 array buffer (but some models require float32 format) Float32Array///Uint8Array
             const arrayBuffer = new Float32Array(finalArray);
-            const result = actualModel?.runSync([arrayBuffer])
-             console.log(result?.length)
-            // Assuming `result` is the output tensor from the model with shape [1,5,8400]
+            const result = actualModel?.runSync([arrayBuffer]);
             if (!result || result.length < 1) return;
-            const outputTensor = result[0]; // Access the tensor
-            console.log(outputTensor?.length)
-            const numDetections = 8400; // Total number of predictions
-            const Alldetections: any = [];
-
+            console.log(result?.length);
+            const outputTensor = result[0];
+            console.log(outputTensor?.length);
+            const numDetections = 8400;
             const targetWidth = 640;
             const targetHeight = 640;
-        
-            for (let i = 0; i < numDetections; i++) {
-              const cx = Number(outputTensor[i]);
-              const cy = Number(outputTensor[i + 8400 * 1]);
-              const w = Number(outputTensor[i + 8400 * 2]);
-              const h = Number(outputTensor[i + 8400 * 3]);
-              const x4 = outputTensor[i + 8400 * 4];
-              const x5 = outputTensor[i + 8400 * 5];
-              const x6 = outputTensor[i + 8400 * 6];
-              const x7 = outputTensor[i + 8400 * 7];
-              const x8 = outputTensor[i + 8400 * 8];
-              const x9 = outputTensor[i + 8400 * 9];
-              const y10 = outputTensor[i + 8400 * 10];
-              const y11 = outputTensor[i + 8400 * 11];
-              const y12 = outputTensor[i + 8400 * 12];
-              const y13 = outputTensor[i + 8400 * 13];
-              const y14 = outputTensor[i + 8400 * 14];
-              const y15 = outputTensor[i + 8400 * 15];
-              const y16 = outputTensor[i + 8400 * 16];
-              const y17 = outputTensor[i + 8400 * 17];
-              const y18 = outputTensor[i + 8400 * 16];
-
+            let boxes: any = [];
+            const yolo_classes = ['0', '1', '10', '11', '12', '2', '3', '4', '5', '6', '7', '8', '9', 'n1', 'n2'];
+            for (let index = 0; index < numDetections; index++) {
+              const [class_id, prob] = 
+                [...Array(15).keys()].map((col) => {
+                  return [col, outputTensor[numDetections * (col + 4) + index]]
+                }).reduce((accum, item) => 
+                  item[1] > accum[1] ? item : accum
+                );
               
-              const left = (cx - w / 2) * targetWidth;
-              const top = (cy - h / 2) * targetHeight;
-              const right = (cx + w / 2) * targetWidth;
-              const bottom = (cy + h / 2) * targetHeight;
-              if (x4 != 0) Alldetections.push({
-                coordinate: {
-                  cx,
-                  cy,
-                  w,
-                  h,
+              if (prob < 0.5) {
+                continue;
+              }
+              const label = yolo_classes[class_id as number];
+              const xc = outputTensor[index] as number;
+              const yc = outputTensor[numDetections + index] as number;
+              const w = outputTensor[2 * numDetections + index] as number;
+              const h = outputTensor[3 * numDetections + index] as number;
+              const x1 = (xc - w / 2) / targetWidth * imgSize.width;
+              const y1 = (yc - h / 2) / targetHeight * imgSize.height;
+              const x2 = (xc + w / 2) / targetWidth * imgSize.width;
+              const y2 = (yc + h / 2) / targetHeight * imgSize.height;
+              const left = (xc - w / 2) * targetWidth;
+              const top = (yc - h / 2) * targetHeight;
+              const right = (xc + w / 2) * targetWidth;
+              const bottom = (yc + h / 2) * targetHeight;
+              boxes.push({
+                label, 
+                prob,
+                ycoordinate: {
+                  x1, y1, x2, y2 
                 },
-                boundingbox: {  
-                  left,
-                  top,
-                  right,
-                  bottom
+                zposition: {
+                  left, top, right, bottom
                 },
-                x4,
-                x5,
-                x6,
-                x7,
-                x8,
-                x9,
-                y10,
-                y11,
-                y12,
-                y13,
-                y14,
-                y15,
-                y16,
-                y17,
-                y18
-                /* boundingBox: { x, y, width, height },
-                score: confidenceForclass1 */
-              }); 
+              });
             }
-            console.log(Alldetections)
+            boxes = boxes.sort((boxA: any, boxB: any) => boxB[1] - boxA[1]);
+            console.log(boxes);
+            const output = [];
+            while (boxes.length > 0) {
+              output.push(boxes[0]);
+              boxes = boxes.filter((box: any) => iou(boxes[0].ycoordinate, box.ycoordinate) < 0.7);
+            }
+            console.log(output);
           }
         } else {
           console.log(response.errorMessage);
@@ -204,22 +156,9 @@ export default function HomeScreen() {
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
-
-      <ThemedView /* style={styles.buttonContainer} */>
-        <Button /* style={styles.buttonStyle} */ title="Launch gallery" onPress={uploadImage} />
+      <ThemedView>
+        <Button title="Launch gallery" onPress={uploadImage} />
       </ThemedView>
-      {/* <View style={{
-        width: 350,
-        height: 700
-      }}>
-        <Canvas ref={handleCanvas} style={{
-          width: 350,
-          height: 700,
-          borderWidth: 1,
-          borderColor: "red",
-          backgroundColor: "blue",
-        }} />
-      </View> */}
     </ParallaxScrollView>
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,3 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
@@ -6,20 +5,12 @@ const config = getDefaultConfig(__dirname);
 
 config.transformer = {
   ...config.transformer,
-  // babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  // assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   getTransformOptions: async () => ({
     transform: {
-      experimentalImportSupport: false, // false
+      experimentalImportSupport: false,
       inlineRequires: true,
     },
   }),
-};
-//
-config.resolver = {
-  ...config.resolver,
-  //assetExts: ['tflite', ...config.resolver.assetExts],
-  // sourceExts: [...config.resolver.sourceExts, 'svg', 'd.ts'],
 };
 
 config.resolver.assetExts.push("tflite");
