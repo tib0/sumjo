7d17e804aef0b46cc64547e5b8484212aa3b2fcc
Tib0 <4764729+tib0@users.noreply.github.com>
2024-10-28 14:08:13 +0100

    Add accessibility props

--- components/resultModal.tsx
+++ components/resultModal.tsx
@@ -4,19 +4,28 @@ import { StrokeText } from '@charmy.tech/react-native-stroke-text';
 import { TouchableOpacity, Modal } from 'react-native';
 
 export function ResultModal(
-  { isModalVisible, score, onPress }: 
-  { isModalVisible: boolean, score: string, onPress: (() => void) | undefined }
+  { isModalVisible, score, onPress }:
+    { isModalVisible: boolean, score: string, onPress: (() => void) | undefined }
 ) {
   const styles = styleSheet();
 
   return (
     <Modal animationType='none' transparent={true} visible={isModalVisible}>
-      <TouchableOpacity 
-        style={styles.modalContainer} 
-        onPress={onPress} 
+      <TouchableOpacity
+        style={styles.modalContainer}
+        onPress={onPress}
         activeOpacity={1}
+        accessibilityLabel='Result modal container'
+        accessibilityHint='Click anywhere to close this modal containing detection result.'
+        accessibilityActions={[{ name: 'press', label: 'Close this modal' }]}
+        accessibilityValue={{ 'text': 'Your score is ' + score }}
       >
-        <TouchableOpacity style={styles.modal} activeOpacity={1}>
+        <TouchableOpacity
+          style={styles.modal}
+          activeOpacity={1}
+          accessibilityLabel='Result modal'
+          accessibilityValue={{ 'text': 'Your score is ' + score }}
+        >
           <StrokeText
             text={score}
             fontSize={CONTENT_SPACING_Y * 2.75}
