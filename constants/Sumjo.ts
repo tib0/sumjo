13beacc8d14d2c912fa7dcb63afa52ca7fc554f4
Tib0 <4764729+tib0@users.noreply.github.com>
2024-10-30 16:03:22 +0100

    Update treshold

--- constants/Sumjo.ts
+++ constants/Sumjo.ts
@@ -1,8 +1,8 @@
 
 export const SUMJO_CLASSES = ['0', '1', '10', '11', '12', '2', '3', '4', '5', '6', '7', '8', '9', 'n1', 'n2'];
 
-export const SUMJO_PROB_TRESHOLD = 0.7;
+export const SUMJO_PROB_TRESHOLD = 0.85;
 
-export const SUMJO_IOU_TRESHOLD = 0.5;
+export const SUMJO_IOU_TRESHOLD = 0.9;
 
 export const SUMJO_DETECTIONS = 8400;
